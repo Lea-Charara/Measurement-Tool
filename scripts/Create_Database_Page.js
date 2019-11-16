29e6e80bfa9cb467dab9263985d3325aa0d52670
Lea-Charara <leacharara@hotmail.com>
2019-11-16 13:11:34 +0200

    useless var removed

--- scripts/Create_Database_Page.js
+++ scripts/Create_Database_Page.js
@@ -1,3 +1,5 @@
+
+
 function myFunction() {
     var x = document.getElementById("myInput");
     if (x.type === "password") {
@@ -19,18 +21,18 @@ function myFunction() {
     function CheckFields(){
     
     var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    var a = document.getElementById("myInput");
-    var b = document.getElementById("InputUser");
-    var c = document.getElementById("InputPort");
-    var d = document.getElementById("InputHost");
-    var e = document.getElementById("InputName");
-    var IpAddr=d.value;
-   if (a.value.length == 0 || b.value.length == 0 || e.value.length == 0)
+    var pass = document.getElementById("myInput");
+    var username = document.getElementById("InputUser");
+    var port = document.getElementById("InputPort");
+    var host = document.getElementById("InputHost");
+    var name = document.getElementById("InputName");
+    var IpAddr=host.value;
+   if (pass.value.length == 0 || username.value.length == 0 || name.value.length == 0)
    {
          alert("Not all fields have been entered")
          
    }
-   else if(isNaN(c.value)) 
+   else if(isNaN(port.value)) 
    {
          alert("Port Field is not a valid number")
    }
@@ -40,10 +42,11 @@ function myFunction() {
    }
    else
    {
-         console.log(CheckedType())
+         
+         
       $.ajax({
             type: "POST",
-            url: "measurementtoolbackend.herokuapp.com/databases/adddatabase/",
+            url: "https://measurementtoolbackend.herokuapp.com/databases/adddatabase/",
             // The key needs to match your method's input parameter (case-sensitive).
             data:JSON.stringify({name:document.getElementById("InputName").value,
             user: document.getElementById("InputUser").value,
@@ -51,17 +54,16 @@ function myFunction() {
             password: document.getElementById("myInput").value,
             host: document.getElementById("InputHost").value,
             port:  document.getElementById("InputPort").value}),
-            
-      
-                  
             contentType: "application/json; charset=utf-8",
             success: function(){
                   var x = document.getElementById("snackbar");
                   x.className = "show";
                   setTimeout(function(){ x.className = x.className.replace("show", ""); window.location.href = 'Databases.html'; }, 1500);
+    },
+    error: function(){
+          alert("Something went wrong. Make sure that all the field are correctly entered.")
+    }
     
-
-   }
 
  }
       )}}
