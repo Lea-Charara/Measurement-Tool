f240101d8fc7bc8f6bb3093dbbceb8ee4c758f3b
Moudiz <mohamadkerdly@gmail.com>
2019-12-06 23:17:32 +0200

    * Progress label should no longer affect the position of everything around it. * Tests are displayed correctly after refresh. + Delete button WIP.

--- scripts/tests.js
+++ scripts/tests.js
@@ -58,6 +58,11 @@ function StartTest(test_id){
         {
             clearInterval(intervals[test_id]);
             intervals[test_id] = null;
+            $(elems).find("#start").hide();
+            $(elems).find("#pause").hide();
+            $(elems).find("#stop").hide();
+            $(elems).find("#edit").attr("disabled", false);
+            $(elems).find("#restart").show();
         } 
     }, 1);
 }
@@ -65,6 +70,18 @@ function StartTest(test_id){
         console.log(e);
     }
 }
+
+//  Restart
+function RestartTest(test_id) {
+    // Placeholder
+    var elems = $("#test-"+test_id).children();
+    $(elems).find("#start").show();
+    $(elems).find("#pause").show();
+    $(elems).find("#stop").show();
+    $(elems).find("#edit").attr("disabled", true);
+    $(elems).find("#restart").hide();
+}
+
 
 //  Pause Button
 function PauseTest(test_id) {
@@ -146,9 +163,12 @@ $(window).on("load",function(){
                 $("#tests").show();
                 $("#newTest").show();
                 for(i = 0;i < response.length;i++){
-                    var test = response[i];
-                    $("#tests").append('<div class ="test" id="test-'+test.id+'"><p>'+test.name+'</p><div class="inner"><div class="loadbar w3-round-xlarge" id="barDiv" style="width: 70%"><div id="bar" class="loadbar w3-round-xlarge" style="width:'+test.Progress+'%;height: 20px; padding:0"></div></div>&emsp;<p id="prog">'+test.Progress+'%</p>&emsp;<button type="button" id="start" class="button" onclick="StartTest('+test.id+')"><i class="fa fa-play" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="pause" class="button" onclick="PauseTest('+test.id+')" disabled><i class="fa fa-pause" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="stop" class="button" onclick="StopTest('+test.id+')" disabled><i class="fa fa-stop" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button>&emsp;&emsp;<button type="button" id="edit" class="button" onClick="Edit('+test.id+')"><i class="fa fa-edit" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="view" class="button" onClick="View('+test.id+')"><i class="fa fa-eye" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button></div>');
+                    var test = response[i]; //style="padding-left: 89%;"
                     intervals[test.id]=null;
+
+                    $("#tests").append('<div class ="test" id="test-'+test.id+'"><div class="up"><p>'+test.name+'</p><button type="button" id="view" class="button" onclick="Delete(1)" style="float: right"><i class="fa fa-times" style="font-size:20px;text-shadow:5px 4px 6px #000000; color: red;"></i></button></div><div class="inner"><div class="loadbar w3-round-xlarge" id="barDiv" style="width: 70%"><div id="bar" class="'+((test.Progress > 0 && test.Progress < 100)? "paused" : (test.Progress==100)?"started":"loadbar")+' w3-round-xlarge" style="width:'+test.Progress+'%;height: 20px; padding:0"></div></div>&emsp;<span id="prog" style="width:5%;">'+test.Progress+'%</span>&emsp;<button type="button" id="start" class="button" onclick="StartTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':'')+'><i class="fa fa-play" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="pause" class="button" onclick="PauseTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':'')+' disabled><i class="fa fa-pause" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="stop" class="button" onclick="StopTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':(test.Progress == 0)?"disabled":'')+'><i class="fa fa-stop" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="restart" class="button" onclick="RestartTest('+test.id+')"'+ ((test.Progress != 100)?'style="display:none;"':'')+'><i class="fa fa-repeat" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button>&emsp;&emsp;<button type="button" id="edit" class="button" onClick="Edit('+test.id+')"'+((test.Progress > 0 && test.Progress < 100)? "disabled":"")+'><i class="fa fa-edit" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="view" class="button" onClick="View('+test.id+')"><i class="fa fa-eye" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button></div>');
+                    if(test.Progress > 0 && test.Progress < 100)
+                    PauseTest(test.id);
                 }
             }
             $(".tests").mCustomScrollbar({
