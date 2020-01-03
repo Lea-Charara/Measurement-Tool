ead15656c1c470fb48268f43d6cf7b79f1916a51
“incigroupe4” <elia.chayban@net.usj.edu.lb>
2020-01-03 19:33:21 +0200

    Linked  function to front end

--- scripts/View_Tests.js
+++ scripts/View_Tests.js
@@ -29,16 +29,16 @@ $(window).on('load',function(){
           var dateTime = date;
          
          $("#Database_name").append('<div  class="col"><p>'+response.db_name[i]+'</p><br/><div class="column Last"align="Center"><p align="left" id="rcorners1">'+response.test_query[i]+'</p><br/><br/><table style="width: 100%; height: 50%" ><tr><th>Loading</th> <th>Time</th></tr><tr><td><div class="loadbar w3-round-xlarge" id="barDiv" style="width: 100%;height:20px"><div id="bar" class="started w3-round-xlarge" style="width:50%;height: 20px"></div></div></td><td align="center" id="time" >'+response.Test_Duration[i]+'</td></tr></table><div class="row"><div id="done date" class="colu" ><h2>Test date</h2><p>'+dateTime+'</p></div><div id="Repetition" class="colu" ><h2>Repetition</h2><p>'+response.NB_Query[i]+'</p></div><div id="Average" class="colu" ><h2>Average time of the query</h2><p>'+response.average[i]+'</p></div></div></div></div>');
-        
+          UpdateTest(id);
          }
     }
 
-
+  
+   
 
 
 
   });
-
   
 
 });
@@ -51,4 +51,60 @@ function openTab(tabName) {
     x[i].style.display = "none";
   }
   document.getElementById(tabName).style.display = "block";
+}
+
+function UpdateTest(id){
+  var done =0;
+  $.ajax({
+    url: "https://127.0.0.1:8000/dbtests/GetProgress/",
+    type:"POST",
+    data:{testid: id},
+    dataType:"json",
+
+    success: function(response) {
+
+      done = response.prog;
+      
+    },
+    error:function(errors){
+      if(errors.status==400){
+        connectionError();
+      }
+      if(errors.status==404){
+        DeletedTestError();
+      }
+      
+     $(bar).css('width', done + '%');
+      
+    }
+    
+
+  });
+
+}
+
+
+function connectionError(){
+  if(!err){
+      err = true;
+      Swal.fire({
+          title: `Connection Error`,
+          html: 'Please check your internet connection and try again.<br><br><b>The page will now reload.</b>'
+      }).then(() => {
+          window.location.reload()
+      });
+  }
+}
+
+
+function DeletedTestError(){
+  if(!err){
+      err = true;
+      Swal.fire({
+          title: `The test has been deleted`,
+          html: 'The page will now reload.</b>'
+      }).then(() => {
+          window.location.reload()
+      });
+  }
 }
