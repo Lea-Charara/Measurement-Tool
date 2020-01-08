b06fb627a743e136ea2623da056d33fc648c2850
Moudiz <mohamadkerdly@gmail.com>
2020-01-08 14:10:30 +0200

    * View test is now disabled when test is paused or complete.

--- scripts/tests.js
+++ scripts/tests.js
@@ -95,6 +95,7 @@ function StartTest(test_id){
     $(elems).find("#stop").attr("disabled", false);
     $(elems).find("#delete").attr("disabled", true);
     $(elems).find("#edit").attr("disabled", true);
+    $(elems).find("#view").attr("disabled", true);
     $(bar).removeClass("loadbar paused stopped").addClass("started");
     done = UpdateTest(test_id);
     intervals[test_id] = setInterval(function() {
@@ -111,6 +112,7 @@ function StartTest(test_id){
             $(elems).find("#pause").hide();
             $(elems).find("#stop").hide();
             $(elems).find("#edit").attr("disabled", false);
+            $(elems).find("#view").attr("disabled", false);
             $(elems).find("#restart").show();
         } 
     }, 25);
@@ -142,6 +144,7 @@ function RestartTest(test_id) {
     $(elems).find("#pause").attr("disabled", false);
     $(elems).find("#stop").show();
     $(elems).find("#edit").attr("disabled", true);
+    $(elems).find("#view").attr("disabled", true);
     $(elems).find("#restart").hide();
 
     setTimeout(intervals[test_id] = setInterval(function() {
@@ -159,6 +162,7 @@ function RestartTest(test_id) {
             $(elems).find("#stop").hide();
             $(elems).find("#edit").attr("disabled", false);
             $(elems).find("#restart").show();
+            $(elems).find("#view").attr("disabled", false);
         } 
     }, 25), 355);   
     
@@ -188,6 +192,7 @@ function PauseTest(test_id) {
     $(elems).find("#start").attr("disabled", false);
     $(elems).find("#stop").attr("disabled", false);
     $(elems).find("#delete").attr("disabled", false);
+    $(elems).find("#view").attr("disabled", false);
     clearInterval(intervals[test_id]);
     $(bar).removeClass("loadbar started stopped").addClass("paused");
 }
@@ -218,6 +223,7 @@ function StopTest(test_id) {
     $(elems).find("#pause").attr("disabled", true);
     $(elems).find("#edit").attr("disabled", false);
     $(elems).find("#delete").attr("disabled", false);
+    $(elems).find("#view").attr("disabled", true);
     $(bar).removeClass("loadbar started paused").addClass("stopped");
 
     clearInterval(intervals[test_id]);
@@ -279,7 +285,7 @@ $(window).on("load",function(){
                     var test = response[i];
                     intervals[test.id]=null;
 
-                    $("#tests").append('<div class ="test" id="test-'+test.id+'"><div class="up"><p>'+test.name+'</p><button type="button" id="delete" class="button" onclick="DeleteTest('+test.id+')" style="float: right; color: red;"><i class="fa fa-times" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button></div><div class="inner"><div class="loadbar w3-round-xlarge" id="barDiv" style="width: 70%"><div id="bar" class="'+((test.Progress == 100)?"started":"loadbar")+' w3-round-xlarge" style="width:'+test.Progress+'%;height: 20px; padding:0"></div></div>&emsp;<span id="prog" style="width:5%;">'+test.Progress+'%</span>&emsp;<button type="button" id="start" class="button" onclick="StartTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':'')+'><i class="fa fa-play" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="pause" class="button" onclick="PauseTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':'')+' disabled><i class="fa fa-pause" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="stop" class="button" onclick="StopTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':(test.Progress == 0)?"disabled":'')+'><i class="fa fa-stop" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="restart" class="button" onclick="RestartTest('+test.id+')"'+ ((test.Progress != 100)?'style="display:none;"':'')+'><i class="fa fa-repeat" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button>&emsp;&emsp;<button type="button" id="edit" class="button" onClick="Edit('+test.id+')"><i class="fa fa-edit" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="view" class="button" onClick="View('+test.id+')"><i class="fa fa-eye" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button></div>');
+                    $("#tests").append('<div class ="test" id="test-'+test.id+'"><div class="up"><p>'+test.name+'</p><button type="button" id="delete" class="button" onclick="DeleteTest('+test.id+')" style="float: right; color: red;"><i class="fa fa-times" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button></div><div class="inner"><div class="loadbar w3-round-xlarge" id="barDiv" style="width: 70%"><div id="bar" class="'+((test.Progress == 100)?"started":"loadbar")+' w3-round-xlarge" style="width:'+test.Progress+'%;height: 20px; padding:0"></div></div>&emsp;<span id="prog" style="width:5%;">'+test.Progress+'%</span>&emsp;<button type="button" id="start" class="button" onclick="StartTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':'')+'><i class="fa fa-play" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="pause" class="button" onclick="PauseTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':'')+' disabled><i class="fa fa-pause" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="stop" class="button" onclick="StopTest('+test.id+')"'+((test.Progress == 100)?'style="display:none;"':(test.Progress == 0)?"disabled":'')+'><i class="fa fa-stop" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="restart" class="button" onclick="RestartTest('+test.id+')"'+ ((test.Progress != 100)?'style="display:none;"':'')+'><i class="fa fa-repeat" style="font-size:17px;text-shadow:5px 4px 6px #000000;"></i></button>&emsp;&emsp;<button type="button" id="edit" class="button" onClick="Edit('+test.id+')"><i class="fa fa-edit" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button><button type="button" id="view" class="button" onClick="View('+test.id+')"'+ ((test.Progress == 100 || test.Status == 2)?"enabled":'disabled') +'><i class="fa fa-eye" style="font-size:20px;text-shadow:5px 4px 6px #000000;"></i></button></div>');
                     
                     if(test.Status == 1)
                         PauseTest(test.id);
